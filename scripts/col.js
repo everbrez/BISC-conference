2ab53cc55365388eee793f52261a9cce0b263259
zhengpq <>
2019-05-30 21:45:11 +0800

    [index] 添加跳转失败处理方法，删除col.js多余代码

--- scripts/col.js
+++ scripts/col.js
@@ -1,23 +1,4 @@
 window.addEventListener('load', function() {
-  // sidebar affix
-  // const sidebar = document.getElementsByClassName('sidebar')[0];
-  // if (sidebar) {
-  //   const offsetTop = sidebar.offsetTop;
-  //   const padding = 40;
-  //   affix(sidebar, offsetTop, padding);
-  //   document.addEventListener('scroll', affix.bind(this, sidebar, offsetTop, padding), false);
-  //   document.addEventListener('resize', affix.bind(this, sidebar, offsetTop, padding), false);
-  // }
-
-  // function affix(sidebar, offsetTop, padding) {
-  //   if (offsetTop - padding <= pageYOffset) {
-  //     sidebar.style.position = 'fixed';
-  //     sidebar.style.top = padding + 'px';
-  //   } else {
-  //     sidebar.style.position = 'static';
-  //   }
-  // }
-
   // nav-phone ver
   const navOpen = document.getElementById('nav-open');
   const navClose = document.getElementById('nav-close');
@@ -35,6 +16,3 @@ window.addEventListener('load', function() {
   }, false);
 
 }, false)
-
-<img(.+)/>([\n\w\W]+?)("guest-name">)(.+)<
-<img$1 alt="$4"/>$2$3$4<
