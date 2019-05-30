9ae3907e94fa7c015501eb75bc417a6b5a6ea0ac
zhengpq <>
2019-05-30 21:11:40 +0800

    [aside] js control -> css sticky

--- scripts/col.js
+++ scripts/col.js
@@ -1,22 +1,22 @@
 window.addEventListener('load', function() {
   // sidebar affix
-  const sidebar = document.getElementsByClassName('sidebar')[0];
-  if (sidebar) {
-    const offsetTop = sidebar.offsetTop;
-    const padding = 40;
-    affix(sidebar, offsetTop, padding);
-    document.addEventListener('scroll', affix.bind(this, sidebar, offsetTop, padding), false);
-    document.addEventListener('resize', affix.bind(this, sidebar, offsetTop, padding), false);
-  }
+  // const sidebar = document.getElementsByClassName('sidebar')[0];
+  // if (sidebar) {
+  //   const offsetTop = sidebar.offsetTop;
+  //   const padding = 40;
+  //   affix(sidebar, offsetTop, padding);
+  //   document.addEventListener('scroll', affix.bind(this, sidebar, offsetTop, padding), false);
+  //   document.addEventListener('resize', affix.bind(this, sidebar, offsetTop, padding), false);
+  // }
 
-  function affix(sidebar, offsetTop, padding) {
-    if (offsetTop - padding <= pageYOffset) {
-      sidebar.style.position = 'fixed';
-      sidebar.style.top = padding + 'px';
-    } else {
-      sidebar.style.position = 'static';
-    }
-  }
+  // function affix(sidebar, offsetTop, padding) {
+  //   if (offsetTop - padding <= pageYOffset) {
+  //     sidebar.style.position = 'fixed';
+  //     sidebar.style.top = padding + 'px';
+  //   } else {
+  //     sidebar.style.position = 'static';
+  //   }
+  // }
 
   // nav-phone ver
   const navOpen = document.getElementById('nav-open');
